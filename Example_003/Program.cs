327850f1ca78e5256bcb743e055925d8bd7fa021
Alyona <nakamura-san@mail.ru>
2022-09-15 20:53:33 +0300

    Example 003

--- Example_003/Program.cs
+++ Example_003/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number1 = 7;
+int number2 = 19;
+int result = number1 + number2;
+Console.WriteLine(result);
